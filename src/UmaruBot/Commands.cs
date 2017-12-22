a20a94e6bc18c3d7378ed5a4e46623ceb4b9f1a1
Edward B. <edward.ryan.bote@gmail.com>
2017-12-22 13:07:26 -0600

    Added commands to generate random number, join voice channel, leave voice channel, and stream mp3 files through voice.

---
+++ src/UmaruBot/Commands.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Interactivity;
+using DSharpPlus.VoiceNext;
+using System.IO;
+using System.Diagnostics;
+
+
+namespace UmaruBot
+{
+    public class Commands
+    {
+        [Command("random")]
+        public async Task Random(CommandContext ctx, int min, int max)
+        {
+            var rnd = new Random();
+            await ctx.RespondAsync($"🎲 Your random number is: {rnd.Next(min, max)}");
+        }
+
+        [Command("join")]
+        public async Task Join(CommandContext ctx)
+        {
+            var vnext = ctx.Client.GetVoiceNextClient();
+
+            var vnc = vnext.GetConnection(ctx.Guild);
+            if (vnc != null)
+                throw new InvalidOperationException("Already connected to a voice channel in this server.");
+
+            var chn = ctx.Member?.VoiceState?.Channel;
+            if (chn == null)
+                throw new InvalidOperationException("You need to be in a voice channel.");
+
+            vnc = await vnext.ConnectAsync(chn);
+            await ctx.RespondAsync("👌");
+        }
+
+        [Command("leave")]
+        public async Task Leave(CommandContext ctx)
+        {
+            var vnext = ctx.Client.GetVoiceNextClient();
+
+            var vnc = vnext.GetConnection(ctx.Guild);
+            if (vnc == null)
+                throw new InvalidOperationException("Not connected to a voice channel in this server.");
+
+            vnc.Disconnect();
+            await ctx.RespondAsync("👌");
+        }
+
+        [Command("play")]
+        public async Task Play(CommandContext ctx, [RemainingText] string file)
+        {
+            var vnext = ctx.Client.GetVoiceNextClient();
+
+            var vnc = vnext.GetConnection(ctx.Guild);
+            if (vnc == null)
+                throw new InvalidOperationException("Not connected to a voice channel in this server.");
+
+            if (!File.Exists(file))
+                throw new FileNotFoundException("File was not found.");
+
+            await ctx.RespondAsync("👌");
+            await vnc.SendSpeakingAsync(true); // send a speaking indicator
+
+            var psi = new ProcessStartInfo
+            {
+                FileName = "ffmpeg",
+                Arguments = $@"-i ""{file}"" -ac 2 -f s16le -ar 48000 pipe:1",
+                RedirectStandardOutput = true,
+                UseShellExecute = false
+            };
+            var ffmpeg = Process.Start(psi);
+            var ffout = ffmpeg.StandardOutput.BaseStream;
+
+            var buff = new byte[3840];
+            var br = 0;
+            while ((br = ffout.Read(buff, 0, buff.Length)) > 0)
+            {
+                if (br < buff.Length) // not a full sample, mute the rest
+                    for (var i = br; i < buff.Length; i++)
+                        buff[i] = 0;
+
+                await vnc.SendAsync(buff, 20);
+            }
+
+            await vnc.SendSpeakingAsync(false); // we're not speaking anymore
+        }
+    }
+}
--- src/UmaruBot/Program.cs
+++ src/UmaruBot/Program.cs
@@ -1,14 +1,52 @@
 ﻿using System;
+using DSharpPlus;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DSharpPlus.Net.WebSocket;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.Interactivity;
+using DSharpPlus.VoiceNext;
 
 namespace UmaruBot
 {
     public class Program
     {
+        static DiscordClient discord;
+        static CommandsNextModule commands;
+        static VoiceNextClient voice;
+        static InteractivityModule interactivity;
+
         public static void Main(string[] args)
         {
+            MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            discord = new DiscordClient(new DiscordConfiguration
+            {
+                Token = "token here",
+                TokenType = TokenType.Bot, 
+                UseInternalLogHandler = true,
+                LogLevel = LogLevel.Debug
+            });
+
+            commands = discord.UseCommandsNext(new CommandsNextConfiguration
+            {
+                StringPrefix = "~"
+            });
+
+            commands.RegisterCommands<Commands>();
+
+            interactivity = discord.UseInteractivity(new InteractivityConfiguration());
+
+            voice = discord.UseVoiceNext();
+
+            discord.SetWebSocketClient<WebSocket4NetCoreClient>();
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
         }
     }
 }
